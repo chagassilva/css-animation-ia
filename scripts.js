f83fcb85a6f5a95c5a928363cad8e71704e4f089
chagassilva <chagassilvati@gmail.com>
2025-07-06 23:39:49 -0300

    Enviando o projeto para o deploy

---
+++ scripts.js
@@ -0,0 +1,58 @@
+/* 
+VÁRIAVEIS - Um pedacinho de memória do computador
+que eu posso guardar o que eu quiser.
+
+FUNCOES
+É um pedacinho de código QUE, só executa 
+Quando é chamado.
+
+documet = HTML
+querySelector = buscar alguém no HTML
+
+fetch - ferramenta para se comunicar com algo fora do codigo
+
+[x] Descobrir quando o botão foi clicado
+[x] Pegar o que foi escrito no Input
+[x] Enviar para o N8N
+[x] Receber o que o N8N Respondeu
+[x] Colocar na Tela o que ele respondeu    
+
+*/
+let webhook = "https://n8n.chagassilva.com/webhook/animacao-css"
+
+// funcao assincrona
+async function cliqueiNoBotao() {
+    let textoInput = document.querySelector(".input-animacao").value
+    let codigo = document.querySelector(".area-codigo")
+    let areaResultado = document.querySelector(".area-resultado")
+    let botao = document.querySelector(".botao-enviar")
+
+    botao.disabled = true // desabilita o botão enquanto espera a resposta
+    botao.innerHTML = "Enviando..." // muda o texto do botão
+    botao.style.backgroundColor = "#cccccc" // muda a cor do botão para indicar que está carregando
+
+    // fetch - 1) O endereco 2) configuracoes 3) os dados
+    // JSON - O formato de dados que usamos na internet
+
+    let resposta = await fetch(webhook, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ pergunta: textoInput })
+    })
+
+    let resultado = await resposta.json()
+
+    let info = JSON.parse(resultado.resposta)
+
+    console.log(info)
+
+    codigo.innerHTML = info.code
+
+    areaResultado.innerHTML = info.preview
+    botao.disabled = false // habilita o botão novamente
+    botao.innerHTML = "Enviar" // volta o texto do botão    
+
+    // Adiciona o estilo ao head do documento
+    document.head.insertAdjacentHTML('beforeend', "<style>"+ info.style +"</style>")
+}
+
